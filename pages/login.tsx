22738cd044de67a90e64065fc71b2bbba9705e14
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-14 17:02:16 +0530

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,48 +3,50 @@ import Link from "next/link";
 
 const Login = (): React.ReactElement => {
   return (
-    <div className="flex items-center p-8">
+    <div className="flex h-screen items-center justify-center">
       <Head>
         <title>Login</title>
       </Head>
-      <div className="m-auto h-fit w-96 space-y-2 rounded-md border border-gray-300 bg-slate-50 p-6 shadow-md">
-        <h1 className="text-2xl font-semibold text-gray-600">Login</h1>
-        <div className="space-y-1">
-          <span className="flex flex-col space-y-1">
-            <label htmlFor="email">Email</label>
-            <input
-              className="border border-gray-400 p-2"
-              type="email"
-              placeholder="johndoe@gmail.com"
-              name=""
-              id=""
-            />
-          </span>
-          <span className="flex flex-col space-y-1">
-            <label htmlFor="password">Password</label>
-            <input
-              className="border border-gray-400 p-2"
-              type="email"
-              placeholder="**********"
-              name=""
-              id=""
-            />
-          </span>
+      <form>
+        <div className="m-auto h-fit w-96 space-y-2 rounded-md border border-gray-300 bg-slate-50 p-6 shadow-md">
+          <h1 className="text-2xl font-semibold text-gray-600">Login</h1>
+          <div className="space-y-1">
+            <span className="flex flex-col space-y-1">
+              <label htmlFor="email">Email</label>
+              <input
+                className="border border-gray-400 p-2"
+                type="email"
+                placeholder="johndoe@gmail.com"
+                name="email"
+                id=""
+              />
+            </span>
+            <span className="flex flex-col space-y-1">
+              <label htmlFor="password">Password</label>
+              <input
+                className="border border-gray-400 p-2"
+                type="password"
+                placeholder="**********"
+                name="password"
+                id=""
+              />
+            </span>
+          </div>
+          <div className="pt-3">
+            <span className="flex flex-col space-y-2">
+              <button className="border border-gray-500 p-2 font-semibold">
+                Login
+              </button>
+              <button className="bg-yellow-800 p-2 font-semibold text-white">
+                Login As Guest
+              </button>
+            </span>
+            <span className="flex justify-center pt-2 font-medium">
+              <Link href="/signup">Create New Account</Link>
+            </span>
+          </div>
         </div>
-        <div className="pt-3">
-          <span className="flex flex-col space-y-2">
-            <button className="border border-gray-800 p-2 font-semibold">
-              Login
-            </button>
-            <button className="bg-yellow-800 p-2 font-semibold text-white">
-              Login As Guest
-            </button>
-          </span>
-          <span className="flex justify-center pt-2 font-medium">
-            <Link href="/signup">Create New Account</Link>
-          </span>
-        </div>
-      </div>
+      </form>
     </div>
   );
 };
