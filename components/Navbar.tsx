dfb208642a13047d65736aaadd038f4757dc281b
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-14 17:30:10 +0530

    feat: added links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = (): React.ReactElement => {
     <div className="fixed left-0 top-0 right-0 z-20 flex h-16 items-center justify-between border-b  border-gray-300 bg-white px-10 shadow-sm">
       <Link href="/">
         <span className="flex items-center gap-1">
-          <small className="text-2xl font-semibold text-yellow-800">
+          <small className="cursor-pointer text-2xl font-semibold text-yellow-800">
             Miaka
           </small>
         </span>
@@ -19,7 +19,9 @@ const Navbar = (): React.ReactElement => {
         className={`sm: fixed bottom-0 flex gap-6 font-semibold text-gray-800 sm:right-0 sm:left-0 sm:z-20 sm:justify-evenly sm:border-t  sm:border-gray-300 sm:p-3 sm:shadow-sm md:static md:border-none md:shadow-none lg:p-0`}
       >
         <li className="flex cursor-pointer items-center justify-center hover:text-yellow-800">
-          HOME
+          <Link href="/">
+            <a>HOME</a>
+          </Link>
         </li>
         <li className="flex cursor-pointer items-center justify-center hover:text-yellow-800">
           PRODUCTS
