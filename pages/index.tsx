6036205093d0c1a747d5ffdabe6077c469b277f1
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-15 22:46:52 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Features from "../components/Features";
 import Slider from "../components/Slider";
 
 const Home: NextPage = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-20 space-y-6">
       <Head>
         <title>Store</title>
       </Head>
-      <Slider />
+      {/* <Slider /> */}
+      <Features />
     </div>
   );
 };
