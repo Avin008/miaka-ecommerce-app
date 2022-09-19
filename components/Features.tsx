627d535d2cdd705ea13a1b9e9c4914cc0adfda60
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-19 12:54:37 +0530

    Update Features.tsx

--- components/Features.tsx
+++ components/Features.tsx
@@ -5,14 +5,17 @@ import {
 } from "react-icons/md";
 const Features = () => {
   return (
-    <div className="mx-auto grid h-fit grid-cols-3  items-center justify-items-center rounded-md border border-gray-100 bg-[#f6f6f6] p-5 text-gray-600 sm:w-full md:w-3/4">
+    <div className="mx-auto grid h-fit grid-cols-3 items-center justify-items-center border border-gray-100 bg-[#EBD96B] p-2 text-gray-600 sm:w-full">
       <span className="flex flex-col items-center justify-center gap-1 p-2">
         <MdOutlineLocalShipping size={25} />{" "}
-        <span className="font-base text-center leading-5"> Free Shipping</span>
+        <span className="text-center font-medium leading-5">
+          {" "}
+          Free Shipping
+        </span>
       </span>
       <span className="flex flex-col items-center justify-center gap-1 p-2">
         <MdOutlinePayments size={25} />{" "}
-        <span className="font-base text-center leading-5">
+        <span className="text-center font-medium leading-5">
           {" "}
           Secure Payment{" "}
         </span>
