e67fcd245957e1d9e0d4dd794088fc90c1fb8e42
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-21 17:15:53 +0530

    style: updated container padding

--- components/Category.tsx
+++ components/Category.tsx
@@ -17,7 +17,7 @@ const data = [
 
 const Category = (): React.ReactElement => {
   return (
-    <div className="mx-auto w-11/12 space-y-5 p-5">
+    <div className="mx-auto w-11/12 space-y-5 p-3">
       <h1 className="text-lg font-bold">CATEGORIES</h1>
       <div className="grid gap-5 sm:grid-cols-1 md:grid-cols-3">
         {data.map((x) => (
