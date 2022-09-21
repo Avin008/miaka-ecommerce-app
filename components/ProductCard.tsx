994c39de73bcf190bcc13dc95fc0a693802832da
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-21 18:33:04 +0530

    feat: added add to cart btn

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { MdArrowRightAlt } from "react-icons/md";
+import { MdArrowRightAlt, MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { useToggle } from "../hooks/useToggle";
 
 type Props = {
   data: {
@@ -10,8 +11,17 @@ type Props = {
 };
 
 const ProductCard = ({ data }: Props): React.ReactElement => {
+  const { toggle, toggleBtn } = useToggle();
+
   return (
-    <div className="h-fit w-full hover:cursor-pointer">
+    <div className="relative h-fit w-full select-none hover:cursor-pointer">
+      <span className="absolute right-5 top-3 z-10 rounded-full bg-gray-50 p-1 opacity-70 shadow-lg transition-transform hover:bg-gray-300 hover:text-gray-900">
+        {toggle ? (
+          <MdFavorite size={25} onClick={toggleBtn} />
+        ) : (
+          <MdFavoriteBorder size={25} onClick={toggleBtn} />
+        )}
+      </span>
       <div className="relative h-40 w-full">
         <Image
           src={data.img}
