54c9251f0ba25babd040b70bdb7023beee90908a
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-23 11:03:15 +0530

    Update: added key props

--- components/SelectSize.tsx
+++ components/SelectSize.tsx
@@ -9,6 +9,7 @@ const SelectSize = (): React.ReactElement => {
     <div className="absolute bottom-0 right-0 left-0 z-10 flex items-center justify-around bg-gray-300 p-2 text-sm font-medium text-gray-800 opacity-75 transition-all">
       {sizes.map((x) => (
         <span
+          key={x}
           className={`${
             x === size && `bg-gray-800 text-white opacity-100`
           } flex items-center justify-center rounded-full border border-gray-800 p-4 transition-all`}
