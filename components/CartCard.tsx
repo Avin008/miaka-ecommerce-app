0e4165eb954c18a2e859a84b7e5267a2d35a932a
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-09-28 20:38:00 +0530

    feat: added cart page

---
+++ components/CartCard.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import { useState } from "react";
+import {
+  MdClose,
+  MdKeyboardArrowUp,
+  MdKeyboardArrowDown,
+} from "react-icons/md";
+
+type Props = {
+  data: {
+    img: string;
+    price: number;
+    name: string;
+  };
+};
+
+const CartCard = ({ data }: Props): React.ReactElement => {
+  const [itemQty, setItemQty] = useState<number>(1);
+
+  return (
+    <div className="relative grid h-36 w-full grid-cols-6 gap-2 rounded-md border border-gray-600">
+      <span className="absolute right-2 top-2 cursor-pointer rounded-full border border-gray-100 p-1 hover:bg-gray-50">
+        <MdClose size={22} />
+      </span>
+      <div className="col-span-2 p-2">
+        <div className="relative h-full w-full">
+          <Image
+            className="rounded-md"
+            src={data.img}
+            objectFit="cover"
+            layout="fill"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="rounded-r-m flex items-center text-sm font-medium">
+        {data.name}
+      </div>
+      <div className="flex select-none items-center justify-center gap-2">
+        <span
+          className="cursor-pointer text-lg font-semibold hover:text-gray-700 active:-translate-y-1"
+          onClick={() => setItemQty((prev) => (prev += 1))}
+        >
+          <MdKeyboardArrowUp />
+        </span>
+        <span className="font-base flex h-6 w-6 items-center justify-center border border-gray-300 bg-gray-200 text-base">
+          {itemQty}
+        </span>
+        <span
+          className="cursor-pointer text-lg font-semibold active:-translate-y-1"
+          onClick={() =>
+            setItemQty((prev) => {
+              if (prev > 1) {
+                return (prev -= 1);
+              } else {
+                return prev;
+              }
+            })
+          }
+        >
+          <MdKeyboardArrowDown />
+        </span>
+      </div>
+      <div className="rounded-r-m font-sm flex items-center text-sm font-medium text-gray-700">
+        Size: {"M"}
+      </div>
+      <div className="rounded-r-m font-sm col-span-1 flex items-center text-sm font-medium text-gray-700">
+        Rs.{data.price}
+      </div>
+    </div>
+  );
+};
+
+export default CartCard;
