f0e721c3bfe6b122786a0805fff1b109c3d6af7d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-07 20:21:01 +0530

    Create firebaseFunc.ts

---
+++ services/firebaseFunc.ts
@@ -0,0 +1,62 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
+import { auth, db } from "./firebaseConfig";
+
+const signupFunc = async (email: string, password: string): Promise<string> => {
+  const res = await createUserWithEmailAndPassword(auth, email, password);
+  return res.user.uid;
+};
+
+const loginFunc = async (email: string, password: string): Promise<string> => {
+  const res = await signInWithEmailAndPassword(auth, email, password);
+  return res.user.uid;
+};
+
+const initiateUserData = async (
+  userUID: string,
+  email: string,
+  firstname: string,
+  lastname: string
+): Promise<void> => {
+  const userDocRef = doc(db, "users", userUID);
+  await setDoc(userDocRef, {
+    id: userUID,
+    email,
+    firstname,
+    lastname,
+    cart: [],
+    wishlist: [],
+    address: {},
+  });
+};
+
+const getCollectionData = async (collectionName: string): Promise<any> => {
+  const collectionRef = collection(db, collectionName);
+  const res = await getDocs(collectionRef);
+  const data = res.docs.map((x) => x.data());
+  return data;
+};
+
+const getSingleDoc = async (collectionName: string, docID: string) => {
+  const docRef = doc(db, collectionName, docID);
+  const res = await getDoc(docRef);
+  const data = res.data();
+  return data;
+};
+
+const logoutFunc = async (): Promise<void> => {
+  await signOut(auth);
+};
+
+export {
+  signupFunc,
+  loginFunc,
+  initiateUserData,
+  getCollectionData,
+  getSingleDoc,
+  logoutFunc,
+};
