5f3941f4f54d0902c9abbb03c4b36fb8894296f1
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-12 21:17:49 +0530

    Update Category.tsx

--- components/Category.tsx
+++ components/Category.tsx
@@ -1,14 +1,12 @@
 import CategoryCard from "./CategoryCard";
 
 type Props = {
-  data:
-    | {
-        id: string;
-        name: string;
-        img: string;
-        route: string;
-      }[]
-    | undefined;
+  data: {
+    id: string;
+    name: string;
+    img: string;
+    route: string;
+  }[];
 };
 
 const Category = ({ data }: Props): React.ReactElement => {
@@ -16,7 +14,7 @@ const Category = ({ data }: Props): React.ReactElement => {
     <div className="mx-auto w-11/12 space-y-5 p-3">
       <h1 className="text-lg font-bold">CATEGORIES</h1>
       <div className="grid gap-5 sm:grid-cols-1 md:grid-cols-3">
-        {data?.map((x) => (
+        {data.map((x) => (
           <CategoryCard key={x.id} data={x} />
         ))}
       </div>
