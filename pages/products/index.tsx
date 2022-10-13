1c756f4b8005912e981d24127188fbd170421b7f
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-13 19:27:00 +0530

    update: added products from db

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,9 +1,15 @@
 import Head from "next/head";
 import Filter from "../../components/Filter";
 import Card from "../../components/Card";
-import { products } from "../../components/TrendingProducts";
+import useGetTrendingProductsData from "../../hooks/useGetTrendingProducts";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import { ProductData } from "../../types/types";
 
 const Products = (): React.ReactElement => {
+  const { productsData, isProductsDataLoading } = useGetTrendingProductsData();
+
+  if (isProductsDataLoading) return <LoadingSpinner />;
+
   return (
     <div className="mx-auto mt-20 grid h-fit w-11/12 grid-cols-4 gap-10">
       <Head>
@@ -16,8 +22,8 @@ const Products = (): React.ReactElement => {
         </span>
 
         <div className="grid h-full gap-6 sm:col-span-4 sm:grid-cols-2 md:grid-cols-3 lg:col-span-3">
-          {products.map((x) => (
-            <Card key={x.id} data={x} />
+          {productsData.map((productData: ProductData) => (
+            <Card key={productData.id} data={productData} />
           ))}
         </div>
       </>
