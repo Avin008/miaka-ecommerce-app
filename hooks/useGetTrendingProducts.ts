d599ad21d2856d6d472fe44763434a04b3070d7d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-13 18:40:23 +0530

    Create useGetTrendingProducts.ts

---
+++ hooks/useGetTrendingProducts.ts
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { DocumentData } from "firebase/firestore";
+import { getCollectionData } from "../services/firebaseFunc";
+import { ProductData } from "../types/types";
+
+const useGetTrendingProductsData = (): {
+  productsData: any;
+  isProductsDataLoading: boolean;
+  isProductsDataError: boolean;
+} => {
+  const {
+    data: productsData,
+    isLoading: isProductsDataLoading,
+    isError: isProductsDataError,
+  } = useQuery(["products"], async () => {
+    return getCollectionData("products");
+  });
+
+  return { productsData, isProductsDataLoading, isProductsDataError };
+};
+
+export default useGetTrendingProductsData;
