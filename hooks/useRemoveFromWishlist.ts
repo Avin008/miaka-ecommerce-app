5a97e307a8c4b7eac467b8de67800f58702e3e6c
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-13 19:21:43 +0530

    Create useRemoveFromWishlist.ts

---
+++ hooks/useRemoveFromWishlist.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { removeFromWishlist } from "../services/firebaseFunc";
+import { ProductData } from "../types/types";
+
+const useRemoveFromWishlist = (
+  productData: ProductData,
+  userID: string | undefined
+) => {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: removeFromWishlistFunc,
+    isLoading: isRemoveFromWishlistLoading,
+    isError: isRemoveFromWishlistError,
+  } = useMutation(
+    async () => {
+      return removeFromWishlist(userID!, productData);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["user"]);
+      },
+    }
+  );
+
+  return {
+    removeFromWishlistFunc,
+    isRemoveFromWishlistLoading,
+    isRemoveFromWishlistError,
+  };
+};
+
+export default useRemoveFromWishlist;
