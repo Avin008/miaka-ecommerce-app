652664cb4bf43a3aac50bb1c38c617fd523738ba
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-15 12:19:35 +0530

    update: added price filter type

--- types/types.ts
+++ types/types.ts
@@ -30,3 +30,15 @@ export type CartCardProps = {
   productData: ProductData;
   userData?: UserData | undefined;
 };
+
+export type FilterInitialState = {
+  category: string[];
+  ratings: number;
+  sort_by: string;
+  price: number;
+  selectCategory: (categoryName: string) => void;
+  selectRatings: (ratings: number) => void;
+  selectSortBy: (sortType: string) => void;
+  selectPrice: (price: number) => void;
+  clearAllFilters: () => void;
+};
