57786b77bf65b4148126023d1e08183fdcaf4089
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-16 23:41:06 +0530

    refactor: added index.ts file for components

---
+++ components/index.ts
@@ -0,0 +1,41 @@
+import Card from "./Card";
+import Cart from "./Cart";
+import CartCard from "./CartCard";
+import Category from "./Category";
+import CategoryCard from "./CategoryCard";
+import CheckoutCard from "./CheckoutCard";
+import EmptyWishlist from "./EmptyWishlist";
+import Features from "./Features";
+import Filter from "./Filter";
+import Layout from "./Layout";
+import LoadingSpinner from "./LoadingSpinner";
+import Navbar from "./Navbar";
+import ProductCard from "./ProductCard";
+import Search from "./Search";
+import SelectSize from "./SelectSize";
+import Slider from "./Slider";
+import TrendingProducts from "./TrendingProducts";
+import User from "./User";
+import Wishlist from "./Wishlist";
+
+export {
+  Card,
+  Cart,
+  CartCard,
+  Category,
+  CategoryCard,
+  CheckoutCard,
+  EmptyWishlist,
+  Features,
+  Filter,
+  Layout,
+  LoadingSpinner,
+  Navbar,
+  ProductCard,
+  Search,
+  SelectSize,
+  Slider,
+  TrendingProducts,
+  User,
+  Wishlist,
+};
