c5355864306edf786c73409c54c036719a2eedae
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-20 15:39:24 +0530

    added user-store

---
+++ lib/store/useUserStore.ts
@@ -0,0 +1,19 @@
+import create from "zustand";
+import { ProductData, UserData } from "../../types";
+
+type UserDataExtends = UserData & {
+  addToWishlist: (productData: ProductData[]) => void;
+  removeFromWishlist: (productData: ProductData[]) => void;
+};
+
+const useUserStore = create<UserDataExtends>((set) => ({
+  id: "",
+  firstname: "",
+  lastname: "",
+  email: "",
+  wishlist: [],
+  cart: [],
+  address: {},
+  addToWishlist: (productData) => set({ wishlist: productData }),
+  removeFromWishlist: (productData) => set({ wishlist: productData }),
+}));
