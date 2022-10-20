a4804e5b8b724caf672edae44ebb350d309378b3
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-20 13:39:54 +0530

    refactor: added index file for services

---
+++ services/index.ts
@@ -0,0 +1,25 @@
+import {
+  signupFunc,
+  loginFunc,
+  initiateUserData,
+  getCollectionData,
+  getSingleDoc,
+  signoutFunc,
+  addToWishlist,
+  removeFromWishlist,
+  addToCart,
+  removeFromCart,
+} from "../services/firebaseFunc";
+
+export {
+  signupFunc,
+  loginFunc,
+  initiateUserData,
+  getCollectionData,
+  getSingleDoc,
+  signoutFunc,
+  addToWishlist,
+  removeFromWishlist,
+  addToCart,
+  removeFromCart,
+};
