42be0740324504a35f088688bc35028194419202
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-10-20 15:08:10 +0530

    refactor: imported from index

--- hooks/useAuthStatus.ts
+++ hooks/useAuthStatus.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useAuthStore } from "../lib/store";
+
 const useAuthStatus = () => {
   const [isAuth, setIsAuth] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(true);
