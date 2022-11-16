2e3c91ecc00864b4c7a6d6e0d892292cbdd96d78
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-16 22:17:44 +0530

    added active link style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,10 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Cart, Search, User, Wishlist } from "../components";
 
 const Navbar = (): React.ReactElement => {
+  const router = useRouter();
+
   return (
     <div className="fixed left-0 top-0 right-0 z-20 flex h-16 border-b border-gray-300 bg-[#F5F5F5] shadow-sm">
       <span className="mx-auto flex w-11/12 items-center justify-between">
@@ -14,12 +17,20 @@ const Navbar = (): React.ReactElement => {
         <ul
           className={`sm: fixed bottom-0 flex items-center gap-6 border-2 border-black text-gray-800 sm:right-0 sm:left-0 sm:z-20 sm:justify-evenly sm:border-t sm:border-gray-300 sm:bg-[#F5F5F5]  sm:p-3 sm:shadow-sm md:static md:h-full md:border-none md:shadow-none lg:p-0`}
         >
-          <li className="cursor-pointer items-center justify-center hover:text-yellow-800 sm:hidden md:flex">
+          <li
+            className={`cursor-pointer items-center ${
+              router.pathname === "/" && "text-yellow-600"
+            } justify-center hover:text-yellow-800 sm:hidden md:flex`}
+          >
             <Link href="/">
               <a>HOME</a>
             </Link>
           </li>
-          <li className="cursor-pointer items-center justify-center hover:text-yellow-800 sm:hidden md:flex">
+          <li
+            className={`cursor-pointer items-center justify-center hover:text-yellow-800 sm:hidden md:flex ${
+              router.pathname === "/products" && "text-yellow-600"
+            }`}
+          >
             <Link href="/products">
               <a>PRODUCTS</a>
             </Link>
@@ -27,17 +38,29 @@ const Navbar = (): React.ReactElement => {
           <li className="flex cursor-pointer items-center justify-center hover:text-yellow-800">
             <Search />
           </li>
-          <li className="flex cursor-pointer items-center justify-center hover:text-yellow-800">
+          <li
+            className={`flex cursor-pointer items-center justify-center hover:text-yellow-800 ${
+              router.pathname === "/login" && "text-yellow-600"
+            }`}
+          >
             <User />
           </li>
-          <li className="relative flex cursor-pointer items-center justify-center rounded-full border border-gray-900 p-1 hover:text-yellow-800">
+          <li
+            className={`relative flex cursor-pointer items-center justify-center rounded-full border border-gray-900 p-1 hover:text-yellow-800 ${
+              router.pathname === "/wishlist" && "text-yellow-600"
+            }`}
+          >
             <Link href="/wishlist">
               <a>
                 <Wishlist />
               </a>
             </Link>
           </li>
-          <li className="relative flex cursor-pointer items-center justify-center rounded-full border border-gray-900  p-1 hover:text-yellow-800">
+          <li
+            className={`relative flex cursor-pointer items-center justify-center rounded-full border border-gray-900  p-1 hover:text-yellow-800 ${
+              router.pathname === "/cart" && "text-yellow-600"
+            }`}
+          >
             <Link href="/cart">
               <a>
                 <Cart />
