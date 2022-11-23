80ccf5e96c916c90208490bf5c868ba6334647fb
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-23 21:32:19 +0530

    stlye: changed position

--- components/LoadingSpinner.tsx
+++ components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 import { ClipLoader } from "react-spinners";
 const LoadingSpinner = () => {
   return (
-    <div className="absoluteute flex h-screen items-center justify-center">
+    <div className="left-0 right-0 top-0 bottom-0 flex h-screen items-center justify-center">
       <ClipLoader color="black" size={25} />
     </div>
   );
