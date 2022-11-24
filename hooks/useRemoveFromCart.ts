251355f9079f334fc3a842e98fd682fea323da34
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-24 22:33:05 +0530

    added: remove from cart toast

--- hooks/useRemoveFromCart.ts
+++ hooks/useRemoveFromCart.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useAuthStatus } from "../hooks";
 import { removeFromCart } from "../services";
 import { ProductData } from "../types";
+import { toast } from "react-hot-toast";
 
 const useRemoveFromCart = (productData: ProductData) => {
   const { isAuth, authToken } = useAuthStatus();
@@ -19,6 +20,9 @@ const useRemoveFromCart = (productData: ProductData) => {
     {
       onSuccess: () => {
         queryClient.invalidateQueries(["user"]);
+        toast.success("product removed from cart!", {
+          position: "bottom-center",
+        });
       },
     }
   );
