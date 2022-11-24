7a1ed2f7b60c7fb8b20d8fb407238739d0493c11
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-24 22:30:13 +0530

    added: add to cart toast

--- hooks/useAddToCart.ts
+++ hooks/useAddToCart.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useAuthStatus } from "../hooks";
 import { addToCart } from "../services";
 import { ProductData } from "../types";
+import { toast } from "react-hot-toast";
 
 const useAddToCart = (productData: ProductData, size: string | undefined) => {
   const { isAuth, authToken } = useAuthStatus();
@@ -19,6 +20,7 @@ const useAddToCart = (productData: ProductData, size: string | undefined) => {
     {
       onSuccess: () => {
         queryClient.invalidateQueries(["user"]);
+        toast.success("product added to cart!", { position: "bottom-center" });
       },
     }
   );
