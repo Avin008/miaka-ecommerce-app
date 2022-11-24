4fdc6939896a2662df4b60dcb29430e1b626880d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-24 22:27:01 +0530

    added: toast

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "../components";
 import { Layout } from "../types";
+import { Toaster } from "react-hot-toast";
 
 const Layout = ({ children }: Layout) => {
   return (
     <div>
       <Navbar />
       <div className="">{children}</div>
+      <Toaster />
     </div>
   );
 };
