271bfd5342b684a694e46c6876c3504ab215eb43
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-24 08:24:47 +0530

    added env variables

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuwlJpbSkqQl2yXyh7-neascbkDx-BZ6w",
-  authDomain: "miaka-ecommerce-app.firebaseapp.com",
-  projectId: "miaka-ecommerce-app",
-  storageBucket: "miaka-ecommerce-app.appspot.com",
-  messagingSenderId: "809948949200",
-  appId: "1:809948949200:web:fefb961731d8dbe23d8cd5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
