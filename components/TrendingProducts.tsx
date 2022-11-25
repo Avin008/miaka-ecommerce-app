d6da9e587ef7086959a6b5bf03598958409b7b03
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-25 23:06:20 +0530

    style: changed font weight

--- components/TrendingProducts.tsx
+++ components/TrendingProducts.tsx
@@ -13,7 +13,7 @@ const TrendingProducts = (): React.ReactElement => {
 
   return (
     <div className="mx-auto w-11/12 space-y-5 px-3">
-      <h1 className="text-lg font-bold">POPULAR PRODUCTS</h1>
+      <h1 className="text-base font-bold">POPULAR PRODUCTS</h1>
       <div className="grid h-72 justify-items-center gap-5 sm:grid-cols-2 md:grid-cols-3">
         {productsData?.map((productData: ProductData) => (
           <ProductCard
