5942344a8e2b53009f97caf88fc19be692378b5f
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-25 22:59:16 +0530

    added: primary && secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,13 @@ module.exports = {
         sm: "300px",
         md: "550px",
       },
+      colors: {
+        primary: {
+          brand: "#282F3C",
+          hover: "#282F3C",
+        },
+        secondary: "#3F4450",
+      },
     },
   },
   plugins: [scrollbarHide],
