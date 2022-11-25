3a71f33e699119dd031925b18a973c9408676ced
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-25 23:05:24 +0530

    style: changed font weight

--- components/Category.tsx
+++ components/Category.tsx
@@ -4,7 +4,7 @@ import { Category } from "../types";
 const Category = ({ data }: Category): React.ReactElement => {
   return (
     <div className="mx-auto w-11/12 space-y-5 p-3">
-      <h1 className="text-lg font-bold">CATEGORIES</h1>
+      <h1 className="text-base font-bold">CATEGORIES</h1>
       <div className="grid gap-5 sm:grid-cols-1 md:grid-cols-3">
         {data.map((x) => (
           <CategoryCard key={x.id} data={x} />
