f1b48556e9eaf4b479fe1f5a88655889b3b29141
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-27 02:05:47 +0530

    style: changed md grid-col-4

--- components/TrendingProducts.tsx
+++ components/TrendingProducts.tsx
@@ -14,8 +14,8 @@ const TrendingProducts = (): React.ReactElement => {
   return (
     <div className="mx-auto w-11/12 space-y-5 px-3">
       <h1 className="text-base font-bold">POPULAR PRODUCTS</h1>
-      <div className="grid h-72 justify-items-center gap-5 sm:grid-cols-2 md:grid-cols-3">
-        {productsData?.map((productData: ProductData) => (
+      <div className="grid h-fit justify-items-center gap-5 sm:grid-cols-2 md:grid-cols-4">
+        {productsData?.slice(0, 4).map((productData: ProductData) => (
           <ProductCard
             key={productData.id}
             productData={productData}
