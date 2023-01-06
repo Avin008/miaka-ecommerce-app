0600f7a3f5787d75d72e87aea0dce836e2e52196
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-06 15:14:52 +0530

    feat: limit popular products

--- components/TrendingProducts.tsx
+++ components/TrendingProducts.tsx
@@ -1,33 +1,55 @@
-import { LoadingSpinner, ProductCard } from "../components";
-import { useGetTrendingProductsData, useGetUserData } from "../hooks";
+import {
+  LoadingSpinner,
+  ProductCard,
+} from "../components";
+import {
+  useGetTrendingProductsData,
+  useGetUserData,
+} from "../hooks";
 import { useGetCartAndWishlist } from "../hooks";
 import { ProductData } from "../types";
 
-const TrendingProducts = (): React.ReactElement => {
-  const { productsData, isProductsDataLoading } = useGetTrendingProductsData();
-  const { userData, isUserDataLoading } = useGetUserData();
+const TrendingProducts =
+  (): React.ReactElement => {
+    const {
+      productsData,
+      isProductsDataLoading,
+    } = useGetTrendingProductsData();
+    const { userData, isUserDataLoading } =
+      useGetUserData();
 
-  useGetCartAndWishlist(userData, isUserDataLoading);
+    useGetCartAndWishlist(
+      userData,
+      isUserDataLoading
+    );
 
-  if (isProductsDataLoading && isUserDataLoading) return <LoadingSpinner />;
+    if (
+      isProductsDataLoading &&
+      isUserDataLoading
+    )
+      return <LoadingSpinner />;
 
-  return (
-    <div className="mx-auto w-11/12 space-y-5 px-3">
-      <h1 className="text-base font-bold">POPULAR PRODUCTS</h1>
-      <div className="grid h-fit justify-items-center gap-5 sm:grid-cols-2 md:grid-cols-4">
-        {productsData
-          ?.slice(0, 4)
-          .filter((x: ProductData) => x.ratings > 3)
-          .map((productData: ProductData) => (
-            <ProductCard
-              key={productData.id}
-              productData={productData}
-              userData={userData}
-            />
-          ))}
+    return (
+      <div className="mx-auto w-11/12 space-y-5 px-3">
+        <h1 className="text-base font-bold">
+          POPULAR PRODUCTS
+        </h1>
+        <div className="grid h-fit justify-items-center gap-5 sm:grid-cols-2 md:grid-cols-4">
+          {productsData
+            ?.filter(
+              (x: ProductData) => x.ratings > 3
+            )
+            .slice(0, 4)
+            .map((productData: ProductData) => (
+              <ProductCard
+                key={productData.id}
+                productData={productData}
+                userData={userData}
+              />
+            ))}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
 export default TrendingProducts;
