7ae6f2843fc54809f0088e37d5bd79186316d33c
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-06 15:21:20 +0530

    feat: format price to local string en-us

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,7 +5,11 @@ import {
   useAuthStatus,
   useRemoveFromWishlist,
 } from "../hooks";
-import { MdArrowRightAlt, MdFavorite, MdFavoriteBorder } from "../icons";
+import {
+  MdArrowRightAlt,
+  MdFavorite,
+  MdFavoriteBorder,
+} from "../icons";
 import { ProductCardProps } from "../types";
 import { isProductInWishlist } from "../utility";
 
@@ -14,36 +18,50 @@ const ProductCard = ({
   userData,
 }: ProductCardProps): React.ReactElement => {
   const { isAuth } = useAuthStatus();
-  const { addToWishlistFunc } = useAddToWishlist(productData, userData?.id);
-  const { removeFromWishlistFunc } = useRemoveFromWishlist(
+  const { addToWishlistFunc } = useAddToWishlist(
     productData,
     userData?.id
   );
+  const { removeFromWishlistFunc } =
+    useRemoveFromWishlist(
+      productData,
+      userData?.id
+    );
 
   const router = useRouter();
 
   return (
     <div className="relative h-fit w-full select-none">
       <span className="absolute right-5 top-3 z-10 rounded-full bg-gray-50 p-1 opacity-70 shadow-lg transition-transform hover:cursor-pointer hover:bg-gray-300 hover:text-gray-900">
-        {isAuth && isProductInWishlist({ userData, productData }) ? (
+        {isAuth &&
+        isProductInWishlist({
+          userData,
+          productData,
+        }) ? (
           <MdFavorite
             size={25}
             onClick={() => {
-              isAuth ? removeFromWishlistFunc() : router.push("/login");
+              isAuth
+                ? removeFromWishlistFunc()
+                : router.push("/login");
             }}
           />
         ) : (
           <MdFavoriteBorder
             size={25}
             onClick={() => {
-              isAuth ? addToWishlistFunc() : router.push("/login");
+              isAuth
+                ? addToWishlistFunc()
+                : router.push("/login");
             }}
           />
         )}
       </span>
       <div
         className="relative h-40 w-full hover:cursor-pointer"
-        onClick={() => router.push(`product/${productData.id}`)}
+        onClick={() =>
+          router.push(`product/${productData.id}`)
+        }
       >
         <Image
           src={productData.img}
@@ -56,13 +74,21 @@ const ProductCard = ({
       <div className="relative flex flex-col justify-center p-2 leading-5">
         <h1 className="text-sm font-semibold sm:block tab:hidden">
           {productData.name.length > 10
-            ? `${productData.name.slice(0, 10)}...`
+            ? `${productData.name.slice(
+                0,
+                10
+              )}...`
             : productData.name}
         </h1>
         <h1 className="text-sm font-semibold sm:hidden tab:block">
           {productData.name}
         </h1>
-        <h3 className="text-[#7F7F7F]">Rs. {productData.price}</h3>
+        <h3 className="text-[#7F7F7F]">
+          Rs.{" "}
+          {productData.price.toLocaleString(
+            "en-US"
+          )}
+        </h3>
         <MdArrowRightAlt
           size={20}
           color="#797979"
