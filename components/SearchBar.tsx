a2ef2e5d9104b9ec0842c7cb767c2a3dfae89306
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-06 15:50:46 +0530

    style: overflow scroll search resultes

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,10 +4,14 @@ import useSearch from "../hooks/useSearch";
 import { useRouter } from "next/router";
 
 const SearchBar = () => {
-  const [searchKey, setSearchKey] = useState<null | string>(null);
-  const [displaySearchBar, setDisplaySearchBar] = useState<boolean>(false);
+  const [searchKey, setSearchKey] = useState<
+    null | string
+  >(null);
+  const [displaySearchBar, setDisplaySearchBar] =
+    useState<boolean>(false);
 
-  const { data, isLoading, isError } = useSearch(searchKey);
+  const { data, isLoading, isError } =
+    useSearch(searchKey);
 
   const router = useRouter();
 
@@ -21,28 +25,39 @@ const SearchBar = () => {
           type="text"
           placeholder="Search for Products"
           className="w-full border-none bg-transparent p-2 px-3 outline-none ring-gray-900"
-          onChange={(e) => setSearchKey(e.target.value.toLowerCase())}
+          onChange={(e) =>
+            setSearchKey(
+              e.target.value.toLowerCase()
+            )
+          }
           value={searchKey!}
         />
-        <MdSearch size={28} className="absolute right-2 text-gray-600" />
+        <MdSearch
+          size={28}
+          className="absolute right-2 text-gray-600"
+        />
       </div>
-      {displaySearchBar && searchKey && data?.length! > 0 && (
-        <div className="absolute mt-2 w-full space-y-2 rounded-md border border-gray-300 bg-gray-100 p-1 shadow-md">
-          {data?.map((x) => (
-            <div
-              className="flex h-10 cursor-pointer items-center border px-3  font-medium text-gray-900 hover:bg-gray-200 hover:bg-opacity-75"
-              onClick={() => {
-                router.push(`/product/${x.id}`);
-                setSearchKey(x.name.toLowerCase());
-                setDisplaySearchBar(false);
-              }}
-              key={x.id}
-            >
-              {x.name}
-            </div>
-          ))}
-        </div>
-      )}
+      {displaySearchBar &&
+        searchKey &&
+        data?.length! > 0 && (
+          <div className="absolute mt-2 max-h-96 min-h-fit w-full space-y-2 overflow-y-scroll rounded-md border border-gray-300 bg-gray-100 p-1 shadow-md">
+            {data?.map((x) => (
+              <div
+                className="flex h-10 cursor-pointer items-center border px-3  font-medium text-gray-900 hover:bg-gray-200 hover:bg-opacity-75"
+                onClick={() => {
+                  router.push(`/product/${x.id}`);
+                  setSearchKey(
+                    x.name.toLowerCase()
+                  );
+                  setDisplaySearchBar(false);
+                }}
+                key={x.id}
+              >
+                {x.name}
+              </div>
+            ))}
+          </div>
+        )}
     </div>
   );
 };
