7d963ba28cf49257e6a540dcf99221084d8bf01a
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-08 16:56:02 +0530

    style: increased z-index

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Cart, User, Wishlist } from "../components";
+import {
+  Cart,
+  User,
+  Wishlist,
+} from "../components";
 import SearchBar from "./SearchBar";
 
 const Navbar = (): React.ReactElement => {
   const router = useRouter();
 
   return (
-    <div className="fixed left-0 top-0 right-0 z-20 flex h-16 border-b border-gray-300 bg-[#F5F5F5] shadow-sm">
+    <div className="fixed left-0 top-0 right-0 z-40 flex h-16 border-b border-gray-300 bg-[#F5F5F5] shadow-sm">
       <span className="mx-auto flex w-11/12 items-center justify-between">
         <Link href="/">
           <span className="flex items-center gap-1">
@@ -20,11 +24,12 @@ const Navbar = (): React.ReactElement => {
         <SearchBar />
 
         <ul
-          className={`sm: fixed bottom-0 flex items-center gap-6 border-2 border-black text-gray-800 sm:right-0 sm:left-0 sm:z-20 sm:justify-evenly sm:border-t sm:border-gray-300 sm:bg-[#F5F5F5]  sm:p-3 sm:shadow-sm tab:static tab:h-full tab:border-none tab:p-0 tab:shadow-none`}
+          className={`sm: fixed bottom-0 flex items-center gap-6 border-2 border-black text-gray-800 sm:right-0 sm:left-0 sm:z-40 sm:justify-evenly sm:border-t sm:border-gray-300 sm:bg-[#F5F5F5]  sm:p-3 sm:shadow-sm tab:static tab:h-full tab:border-none tab:p-0 tab:shadow-none`}
         >
           <li
             className={`cursor-pointer items-center ${
-              router.pathname === "/" && "font-medium text-primary-brand"
+              router.pathname === "/" &&
+              "font-medium text-primary-brand"
             } justify-center hover:text-primary-hover sm:hidden md:flex`}
           >
             <Link href="/">
@@ -43,7 +48,8 @@ const Navbar = (): React.ReactElement => {
           </li>
           <li
             className={`flex cursor-pointer items-center justify-center hover:text-primary-hover ${
-              router.pathname === "/login" && "font-medium text-primary-brand"
+              router.pathname === "/login" &&
+              "font-medium text-primary-brand"
             }`}
           >
             <User />
@@ -62,7 +68,8 @@ const Navbar = (): React.ReactElement => {
           </li>
           <li
             className={`relative flex cursor-pointer items-center justify-center rounded-full border border-gray-900  p-1 hover:text-primary-hover ${
-              router.pathname === "/cart" && "font-medium text-primary-brand"
+              router.pathname === "/cart" &&
+              "font-medium text-primary-brand"
             }`}
           >
             <Link href="/cart">
